9797ad8b575a18594e0b614d844b53b2ed4b0a72
Daniel Gillis <dtgillis@live.unc.edu>
2014-12-03 10:12:28 -0500

    Added wrapper for running plink recode and parsing .raw files

---
+++ plink/plink_wrapper.py
@@ -0,0 +1,70 @@
+__author__ = 'dtgillis'
+
+import os
+
+
+class SampleGenotype():
+
+    def __init__(self, sample_name, genotype, snp_name):
+
+        self.sample_name = sample_name
+        self.genotype = genotype
+        self.snp_name = snp_name
+
+
+class PlinkExecutableWrapper():
+
+    def __init__(self, plink_executable_path=None):
+
+        self.plink_found = False
+        if plink_executable_path is None:
+            for path in os.environ["PATH"].split(os.pathsep):
+
+                if os.path.isfile(path + os.sep + 'plink'):
+                    self.plink_exec = path + os.sep + 'plink'
+                    self.plink_found = True
+                    print self.plink_exec
+        else:
+            if os.path.isfile(plink_executable_path):
+                self.plink_found = True
+                self.plink_exec = plink_executable_path
+
+        if self.plink_found:
+            print "Found plink executable"
+
+    def extract_snps_recode(self, map_file, ped_file, snp, tmp_dir):
+
+        #form the command for the plink execution
+        cmd = '{0:s} --noweb --ped {1:s} --map {2:s} --map3 --no-pheno --recodeA --snp {3:s}' \
+              ' --out {4:s}{5:s}{3:s}'.format(self.plink_exec, ped_file, map_file, snp, tmp_dir, os.sep)
+        os.system(cmd)
+
+    def parse_snp_recode_raw(self, snp, tmp_dir):
+
+        sample_genotype_list = []
+        recode_file = tmp_dir + os.sep + snp + '.raw'
+        if not os.path.isfile(recode_file):
+            print "error reading plink file {0:s}".format(recode_file)
+            exit(1)
+
+        lines = open(tmp_dir + os.sep + snp + '.raw', 'r').readlines()
+
+        #splice out
+        split_lines = [line.strip(os.linesep).split() for line in lines[1:]]
+
+        for record in split_lines:
+
+            if record[-1] != 'NA':
+                sample_genotype_list.append(SampleGenotype(record[0], int(record[-1]), snp))
+            else:
+                sample_genotype_list.append(SampleGenotype(record[0], -1, snp))
+
+        return sample_genotype_list
+
+    def clean_up_directory(self, tmp_dir, snp):
+
+        file_start = tmp_dir + os.sep + snp
+        for ext in ['.raw', '.nof', '.log']:
+            os.remove(file_start + ext)
+
+
